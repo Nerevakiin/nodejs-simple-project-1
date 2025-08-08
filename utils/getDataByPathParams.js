809dfee5df5765afe4af617ae13011bbd2884cf5
Nerevakiin <djmatisk@yahoo.gr>
2025-08-09 00:32:45 +0300

    updated

--- utils/getDataByPathParams.js
+++ utils/getDataByPathParams.js
@@ -1,6 +1,7 @@
 export const getDataByPathParams = (data, locationType, locationName) => {
-    return data.filter((destination) => {
-      return destination[locationType].toLowerCase() === locationName.toLowerCase()
-    })
+
+  return data.filter((destination) => {
+    return destination[locationType].toLowerCase() === locationName.toLowerCase()
+  })
 
 }
